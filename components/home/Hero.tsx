edbe3995133327ede84a9a61c664d2e6a54e309a
Santhita Krajangwongpaisan <74261236+santhitak@users.noreply.github.com>
2023-03-13 14:22:00 +0700

    feat: components (#3)

---
+++ components/home/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="w-full h-screen bg-[#d3f1ff]">
+      <h1>Hero</h1>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export { default as About } from './home/About';
+export { default as Cases } from './home/Cases';
+export { default as Hero } from './home/Hero';
