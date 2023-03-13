85a8d7527383dec0c91fae176d2945fe358a728c
santhitak <63070175@kmitl.ac.th>
2023-03-13 14:19:20 +0700

    feat: components

---
+++ components/home/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="w-full h-screen bg-[#e4deff]">
+      <h1>About Cira</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/home/Cases.tsx
@@ -0,0 +1,9 @@
+const Cases = () => {
+  return (
+    <div className="w-full h-screen bg-[#ffeed3]">
+      <h1>Use Cases</h1>
+    </div>
+  );
+};
+
+export default Cases;
