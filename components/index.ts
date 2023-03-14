a56e9485f97fd0bc2e4c3c065ef5cf82dd13abf3
santhitak <63070175@kmitl.ac.th>
2023-03-15 04:09:26 +0700

    feat: button components

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,5 @@ export { default as About } from './home/About';
 export { default as Cases } from './home/Cases';
 export { default as Hero } from './home/Hero';
 export { default as Navbar } from './Navbar';
+export { default as OutlinedButton } from './shared/OutlinedButton';
+export { default as PrimaryButton } from './shared/PrimaryButton';
