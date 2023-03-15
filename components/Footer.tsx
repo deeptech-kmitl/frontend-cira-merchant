15af796c3b4e743719d147575f45c9f98ef59e8c
santhitak <63070175@kmitl.ac.th>
2023-03-16 02:59:03 +0700

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,9 @@
 import Link from 'next/link';
 
-interface contacts {
-  svg: SVGElement;
-  href: string;
-}
-
 const Footer = () => {
   return (
-    <div className="w-full bg-gray">
-      <div className="w-full flex justify-between px-20 py-10 space-x-72">
+    <div className="w-full bg-gray divide-[#fcfcfc] divide-y">
+      <div className="w-full flex justify-between px-20 py-14 space-x-72">
         <div className="flex flex-col space-y-2  text-[#FCFCFC]">
           <h4 className="font-semibold text-xl">CIRA ROBOTICS CO., LTD</h4>
           <p className="w-4/5">
@@ -16,7 +11,7 @@ const Footer = () => {
             diska. Jinesade bel när feras redorade i belogi. FAR paratyp i
             muvåning, och pesask vyfisat. Viktiga poddradio har un mad och inde.
           </p>
-          <div className="flex space-x-4">
+          <div className="flex space-x-4 pt-4">
             <Link href="" target="_blank">
               fb
             </Link>
@@ -63,7 +58,7 @@ const Footer = () => {
                 <p>p</p>
                 <p>
                   Office CiRA Lab King Mongkut&apos;s Institute of Technology
-                  Ladkrabang 1 Soi Chalongkrung 1Ladkrabang, Bangkok Thailand
+                  Ladkrabang 1 Soi Chalongkrung 1 Ladkrabang, Bangkok Thailand
                   10520
                 </p>
               </Link>
@@ -71,7 +66,9 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <hr />
+      <div className="w-full flex justify-between px-20 py-6 space-x-72 text-[#fcfcfc]">
+        <p>© 2022 CIRA - Automation and Technology | All Rights Reserved.</p>
+      </div>
     </div>
   );
 };
