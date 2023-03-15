f6999b713416b15dfe0348693ccc9fa1f620e525
nata-non <63070045@kmitl.ac.th>
2023-03-16 06:30:19 +0700

    feat: Navbar and Footer at pricing page

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,11 +1,13 @@
-import { CardPlan } from '@/components';
+import { CardPlan, Footer, Navbar } from '@/components';
 export default function pricing() {
   return (
     <>
+      <Navbar />
       <h1 className="pt-20 text-center font-extrabold text-[46px] text-transparent bg-clip-text bg-gradient-to-r from-[#865F2E] via-[#FCB040] to-[#F9D592]">
         Choose the plan that works for you
       </h1>
       <CardPlan />
+      <Footer />
     </>
   );
 }
