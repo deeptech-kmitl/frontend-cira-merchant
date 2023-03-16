9472cbaa48919045347c2b4c1549d698c6403ab9
Santhita Krajangwongpaisan <74261236+santhitak@users.noreply.github.com>
2023-03-16 12:07:03 +0700

    feat: cta section (#8)
    * feat: cta section
    
    * feat: button as components
    
    * feat: cta section

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,11 @@
 export { default as Footer } from './Footer';
 export { default as About } from './home/About';
 export { default as Cases } from './home/Cases';
+export { default as CTA } from './home/CTA';
 export { default as Hero } from './home/Hero';
 export { default as Support } from './home/Support';
+export { default as GridLayout } from './layout/GridLayout';
 export { default as Navbar } from './Navbar';
 export { default as OutlinedButton } from './shared/OutlinedButton';
 export { default as PrimaryButton } from './shared/PrimaryButton';
+export { default as PrimaryChip } from './shared/PrimaryChip';
