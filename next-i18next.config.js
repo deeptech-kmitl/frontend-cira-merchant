43fd88e326e11741f3d7b6934f009de6a2e6be37
santhitak <63070175@kmitl.ac.th>
2023-03-22 17:20:58 +0700

    feat: i18n

---
+++ next-i18next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'th',
+    locales: ['th', 'en'],
+  },
+  interpolation: {
+    prefix: '{',
+    suffix: '}',
+  },
+  fallbackLng: {
+    default: ['th'],
+  },
+};
