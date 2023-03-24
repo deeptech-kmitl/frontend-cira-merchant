5cf4f079c0b2c41191c98b479783d417e1453dc4
santhitak <63070175@kmitl.ac.th>
2023-03-25 00:37:56 +0700

    feat: forgot password page

--- pages/subscription/forgot-password/index.tsx
+++ pages/subscription/forgot-password/index.tsx
@@ -1,3 +1,44 @@
+import { CardFrame, FloatButton, GridLayout } from '@/components';
+import Image from 'next/image';
+
 export default function ForgotPassword() {
-  return <></>;
+  return (
+    <GridLayout>
+      <div className="w-full h-screen grid grid-cols-2 gap-20 justify-center items-center">
+        <CardFrame>
+          <div className="flex flex-col space-y-4">
+            <Image
+              src="/images/cira_logo.png"
+              alt=""
+              width={130}
+              height={69}
+              style={{ width: '130px', height: 'auto' }}
+            />
+            <h4 className="font-bold text-xl">Forgot your password?</h4>
+            <p className="text-[#737373]">
+              Enter your email associated with your account and we&apos;ll send
+              an email with instructions to reset your password.
+            </p>
+            <input
+              type="text"
+              className="border border-[#D2CFCF] bg-[#F9FAFB] rounded-md p-3 placeholder-text-[#8C939D]"
+              placeholder="example@gmail.com"
+            />
+            <FloatButton>
+              <p>Confirm Email</p>
+            </FloatButton>
+          </div>
+        </CardFrame>
+        <div>
+          <Image
+            src="/images/cira_logo.png"
+            alt=""
+            width={609}
+            height={242}
+            style={{ width: 'auto', height: 'auto' }}
+          />
+        </div>
+      </div>
+    </GridLayout>
+  );
 }
