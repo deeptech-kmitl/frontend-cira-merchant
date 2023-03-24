8fdff0945558a4fdb53bd395e9792ac90209256f
santhitak <63070175@kmitl.ac.th>
2023-03-25 00:51:21 +0700

    feat: forgot password steps

--- components/shared/FloatButton.tsx
+++ components/shared/FloatButton.tsx
@@ -1,10 +1,14 @@
 interface Props {
   children: React.ReactElement;
+  action?: () => void;
 }
 
-const FloatButton = ({ children }: Props) => {
+const FloatButton = ({ children, action }: Props) => {
   return (
-    <button className="bg-[#FCB040] text-white shadow-md shadow-[#FCB040]/30 py-3 rounded-md">
+    <button
+      className="bg-[#FCB040] text-white shadow-md shadow-[#FCB040]/30 py-3 rounded-md"
+      onClick={action}
+    >
       {children}
     </button>
   );
