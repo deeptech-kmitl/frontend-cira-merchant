0c92835958428ffcfd92972d5051f3cb04682ea5
nata-non <63070045@kmitl.ac.th>
2023-03-28 14:38:37 +0700

    feat: checkbox and new design

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,34 +1,42 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
+
 let port = 'http://localhost:3333/v1/auth/login';
 
 const Signin = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const [check, setCheck] = useState(false);
+  const [nameEmpty, setNameEmpty] = useState('hello');
+  const [passwordEmpty, setPasswordEmpty] = useState('hello');
   async function login() {
     let user = {
       identifier: userName,
       password: password,
     };
-    let response = await fetch(port, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Headers': '*',
-      },
-      body: JSON.stringify(user),
-    });
-    const respon = await response.json();
-    console.log(respon);
+    if (userName != '' && password != '') {
+      let response = await fetch(port, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Headers': '*',
+        },
+        body: JSON.stringify(user),
+      });
+      const respon = await response.json();
+      console.log(respon);
+    }
   }
 
   return (
-    <div className="h-screen flex justify-center">
-      <div className="w-full px-12 max-w-[500px] gap-10 h-auto max-h-[700px] flex flex-col items-center shadow-lg">
+    <div className="flex justify-center gap-12 min-h-screen items-center">
+      <div className="p-3 w-full max-w-[800px] lg:p-12 gap-10 max-h-[847px] flex flex-col items-center shadow-lg mb-6">
         <Image src="/images/cira_logo.png" alt="" width={234} height={92} />
+        <h1>Welcome back</h1>
         <button
           type="button"
-          className="flex items-center justify-center text-black bg-[#FFFFFF] text-[24px] shadow-lg hover:shadow-blue-500/40 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium text-sm w-full py-2.5 dark:focus:ring-[#4285F4]/55"
+          className="flex tect-[19px] items-center justify-center text-black bg-[#FFFFFF] text-[24px] shadow-lg hover:shadow-blue-500/40 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium text-sm w-full py-2.5 dark:focus:ring-[#4285F4]/55"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -53,37 +61,74 @@ const Signin = () => {
               d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
             />
           </svg>
-          Login with Google
+          Sign in with Google
         </button>
         <div className="flex items-center gap-2 w-full justify-center">
-          <div className="bg-black hidden sm:block w-[120px] max-w-[120px] h-[1px]"></div>
-          <p className="leading-3">Or login with Email</p>
-          <div className="bg-black hidden sm:block w-[120px] max-w-[120px] h-[1px]"></div>
+          <div className="bg-black hidden sm:block w-[270px] max-w-[270px] h-[1px]"></div>
+          <p className="leading-3 min-w-[150px]">Or login with Email</p>
+          <div className="bg-black hidden sm:block w-[270px] max-w-[270px] h-[1px]"></div>
         </div>
         <div className="w-full flex flex-col gap-5 ">
-          <input
-            type="text"
-            placeholder="Username or Email"
-            onChange={(e) => {
-              setUserName(e.target.value);
-              console.log(userName);
-            }}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-              console.log(password);
-            }}
-          />
+          <div>
+            <input
+              type="text"
+              className="w-full"
+              placeholder="Username or Email"
+              onChange={(e) => {
+                setNameEmpty(e.target.value);
+                setUserName(e.target.value);
+                console.log(userName);
+              }}
+            />
+            {!nameEmpty && (
+              <p className="text-red-600">
+                Please provide your username/email.
+              </p>
+            )}
+          </div>
+          <div>
+            <input
+              className="w-full"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPasswordEmpty(e.target.value);
+                setPassword(e.target.value);
+                console.log(password);
+              }}
+            />
+            {!passwordEmpty && (
+              <p className="text-red-600">Please provide your password.</p>
+            )}
+          </div>
+          <div className="grid grid-cols-2">
+            <div className="flex items-center gap-2">
+              <input
+                type="checkbox"
+                onChange={() => {
+                  setCheck(!check);
+                  console.log(check);
+                }}
+              />
+              <p>Remember me</p>
+            </div>
+            <div className="flex justify-end">
+              <Link href="/" className="relative w-auto hidden lg:block">
+                <h1>Forgot Password ?</h1>
+              </Link>
+            </div>
+          </div>
+
           <button
             onClick={login}
-            className="bg-[#FCB040] py-2.5 text-white hover:bg-white hover:text-[#FCB040] border border-[#FCB040]"
+            className="bg-[#FCB040] py-2.5 text-white hover:bg-[#FFFFFF] hover:text-[#FCB040] border border-[#FCB040]"
           >
-            Login
+            Sign in to your account
           </button>
         </div>
+      </div>
+      <div className="hidden 2xl:flex flex-col justify-center">
+        <Image src="/images/cira_logo.png" alt="" width={609} height={242} />
       </div>
     </div>
   );
