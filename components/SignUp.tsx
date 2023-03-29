8c64e1f5990026a0592c9e4d1d73107d1c3d627d
woranun-wareeparsert <63070155@kmitl.ac.th>
2023-03-30 04:21:11 +0700

    fix: modify

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,51 +1,48 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from 'react';
 import { FloatButton } from '.';
 import Image from 'next/image';
-import Link from 'next/link'
+import Link from 'next/link';
 
 const SignUp = () => {
-
-  const [confirmPassword, setconfirmPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState('');
   const [formData, setFormData] = useState({
-    email: "",
-    fullName: "",
-    phone: "",
-    username: "",
-    password: "",
+    email: '',
+    fullName: '',
+    phone: '',
+    username: '',
+    password: '',
   });
   const [passwordBlank, setpasswordBlank] = useState(false);
   const [checkPassword, setcheckPassword] = useState(false);
   const [policy, setpolicy] = useState(false);
 
-  const handleInput = (e: { target: { name: any; value: any; }; }) => {
+  const handleInput = (e: { target: { name: any; value: any } }) => {
     let fieldName = e.target.name;
     let fieldValue = e.target.value;
 
-    if (fieldName == "phone") {
+    if (fieldName == 'phone') {
       fieldValue = e.target.value.slice(0, 10);
     }
 
     setFormData((prevState) => ({
       ...prevState,
-      [fieldName]: fieldValue
+      [fieldName]: fieldValue,
     }));
-  }
-  console.log(formData)
+  };
+  console.log(formData);
 
   async function handleClick() {
-
-    if (formData.password == "") {
+    if (formData.password == '') {
       setpasswordBlank(true);
     }
     if (!formData.password.match(confirmPassword)) {
       setcheckPassword(true);
-    } 
+    }
     if (!policy) {
       setpolicy(true);
     }
-    
-    await fetch("http://localhost:3333/v1/auth/signup", 
-    {
+
+    await fetch('http://localhost:3333/v1/auth/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -54,8 +51,7 @@ const SignUp = () => {
       body: JSON.stringify(formData),
     });
 
-    await fetch("http://localhost:3333/v1/auth/check-email", 
-    {
+    await fetch('http://localhost:3333/v1/auth/check-email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -70,13 +66,22 @@ const SignUp = () => {
       <div className="grid lg:grid-cols-2">
         <div className="w-11/12 p-12 flex flex-col gap-4 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-[#FFFFFF]">
           <p className="flex text-2xl font-semibold">Create your Account</p>
-          <p className='mb-4 text-[#8C939D]'>Start your website in seconds. Already have an account? 
-            <span className='text-[#D48A3A]'>
+          <p className="mb-4 text-[#8C939D]">
+            Start your website in seconds. Already have an account?
+            <span className="text-[#D48A3A]">
               <Link href="#"> Login here.</Link>
             </span>
           </p>
-          <div id="customBtn" className="w-full flex gap-4 p-3 rounded-xl border border-[#d6d6d6] text-center justify-center items-center">
-            <Image src="/images/google_logo.png" alt="" width={30} height={30} />
+          <div
+            id="customBtn"
+            className="w-full flex gap-4 p-3 rounded-xl border border-[#d6d6d6] text-center justify-center items-center"
+          >
+            <Image
+              src="/images/google_logo.png"
+              alt=""
+              width={30}
+              height={30}
+            />
             <span className="font-medium">Sign up with Google</span>
           </div>
           <div className="relative flex py-5 items-center font-medium">
@@ -86,56 +91,114 @@ const SignUp = () => {
           </div>
           <div className="grid grid-rows-2 gap-6">
             <div className="grid md:grid-cols-2 gap-6">
-              <div className='grid'>
+              <div className="grid">
                 <label className="block mb-2">Your email</label>
-                <input type="text" id="email" name="email" className='rounded-lg mb-2' placeholder="email" value={formData.email} onChange={handleInput} />
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="rounded-lg mb-2"
+                  placeholder="email"
+                  value={formData.email}
+                  onChange={handleInput}
+                />
               </div>
-              <div className='grid'>
+              <div className="grid">
                 <label className="block mb-2">Full name</label>
-                <input type="text" id="fullName" name="fullName" className='rounded-lg' placeholder="name" value={formData.fullName} onChange={handleInput} />
+                <input
+                  type="text"
+                  id="fullName"
+                  name="fullName"
+                  className="rounded-lg"
+                  placeholder="name"
+                  value={formData.fullName}
+                  onChange={handleInput}
+                />
               </div>
             </div>
             <div className="grid md:grid-cols-2 gap-6">
-              <div className='grid'>
+              <div className="grid">
                 <label className="block mb-2">Phone number</label>
-                <input type="number" id="phone" name="phone" className='rounded-lg' placeholder="phone" maxLength={10} value={formData.phone} onChange={handleInput} />
+                <input
+                  type="number"
+                  id="phone"
+                  name="phone"
+                  className="rounded-lg"
+                  placeholder="phone"
+                  maxLength={10}
+                  value={formData.phone}
+                  onChange={handleInput}
+                />
               </div>
-              <div className='grid'>
+              <div className="grid">
                 <label className="block mb-2">Username</label>
-                <input type="text" id="username" name="username" className='rounded-lg' placeholder="username" value={formData.username} onChange={handleInput} />
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="rounded-lg"
+                  placeholder="username"
+                  value={formData.username}
+                  onChange={handleInput}
+                />
               </div>
             </div>
             <div className="grid md:grid-cols-2 gap-6">
-              <div className='grid'>
+              <div className="grid">
                 <label className="block mb-2">Password</label>
-                <input type="password" id="password" name="password" className='rounded-lg' placeholder="•••••••" value={formData.password} onChange={handleInput} />
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="rounded-lg"
+                  placeholder="•••••••"
+                  value={formData.password}
+                  onChange={handleInput}
+                />
               </div>
-              <div className='grid'>
+              <div className="grid">
                 <label className="block mb-2">Confirm Password</label>
-                <input type="password" id="confirmPassword" name="confirmPassword" className='rounded-lg' placeholder="•••••••" value={confirmPassword} onChange={e => setconfirmPassword(e.target.value)} />
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  className="rounded-lg"
+                  placeholder="•••••••"
+                  value={confirmPassword}
+                  onChange={(e) => setconfirmPassword(e.target.value)}
+                />
               </div>
             </div>
           </div>
           <div className="flex gap-2 my-4">
-            <input type='checkbox' className='flex rounded mt-1' onClick={() => setpolicy(true)}/>
-            <p className='text-[#8C939D]'>By signing up, you are creating a Cira core account, and you agree to Cira core’s 
-              <span className='text-[#D48A3A]'>
+            <input
+              type="checkbox"
+              className="flex rounded mt-1"
+              onClick={() => setpolicy(true)}
+            />
+            <p className="text-[#8C939D]">
+              By signing up, you are creating a Cira core account, and you agree
+              to Cira core’s
+              <span className="text-[#D48A3A]">
                 <Link href="#"> Terms of Use</Link>
-              </span> and 
-              <span className='text-[#D48A3A]'>
+              </span>{' '}
+              and
+              <span className="text-[#D48A3A]">
                 <Link href="#"> Privacy Policy.</Link>
               </span>
             </p>
           </div>
           {checkPassword && (
-              <p className="text-red-500">* Password don&apos;t  match</p>
-            )}
+            <p className="text-red-500">* Password don&apos;t match</p>
+          )}
           {policy && (
-              <p className="text-red-500">* You must agree to the Terms of Service</p>
-            )}
+            <p className="text-red-500">
+              * You must agree to the Terms of Service
+            </p>
+          )}
           {passwordBlank && (
-              <p className="text-red-500">* Password can&apos;t be blank</p>
-            )}
+            <p className="text-red-500">* Password can&apos;t be blank</p>
+          )}
           {/* <p className="text-red-500">* Email or username has been used</p>
           <p className="text-red-500">* Email must be an email</p> */}
           <FloatButton action={handleClick}>
@@ -152,7 +215,6 @@ const SignUp = () => {
             alt=""
           />
         </div>
-
       </div>
     </div>
   );
