b3e68bbbae53fa70e3d125bbeb6eb7611cba6822
nata-non <63070045@kmitl.ac.th>
2023-03-30 16:04:57 +0700

    feat: done with design

--- components/order/Payment.tsx
+++ components/order/Payment.tsx
@@ -1,5 +1,6 @@
 import { Country } from '@/data/country';
 import Image from 'next/image';
+import Link from 'next/link';
 import { BsCheck } from 'react-icons/bs';
 
 interface Banking {
@@ -52,7 +53,12 @@ const Payment = () => {
             className="flex space-x-4 items-center py-4 px-5 bg-[#fff] border border-[#C4C4C4] rounded-md"
             key={i}
           >
-            <input type="radio" value={item.value} className="border border-" />
+            <input
+              type="radio"
+              name="bank"
+              value={item.value}
+              className="border border-"
+            />
             <div className="px-5">
               <Image
                 src={item.image}
@@ -64,14 +70,14 @@ const Payment = () => {
           </div>
         ))}
       </div>
-      <div className="bg-[#fff] border border-[#C4C4C4] rounded-md w-full divide-y divide-[#000]/10">
+      <div className="flex flex-col bg-[#fff] border border-[#C4C4C4] rounded-md w-full divide-y divide-[#000]/10">
         <div className="p-6">
-          <div className="flex flex-col space-y-2">
+          <div className="flex flex-col border-[#000000]/10 border-b-2 py-4 gap-y-2">
             <div className="flex justify-between">
               <p className="font-semibold">Professional plan - 24 Month Plan</p>
               <p className="font-medium">฿ 412.83</p>
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between ">
               <p className="flex items-center text-[#404D54] font-medium">
                 <div className="pr-1">
                   <BsCheck className="text-[#26C07D] h-6 w-6" />
@@ -81,7 +87,7 @@ const Payment = () => {
               <p className="font-medium">฿ 42.83</p>
             </div>
           </div>
-          <div className="flex flex-col space-y-2">
+          <div className="flex flex-col py-4 gap-y-2">
             <p className="font-semibold">Country</p>
             <select
               name="country"
@@ -93,7 +99,27 @@ const Payment = () => {
                 </option>
               ))}
             </select>
+            <div className="flex w-full justify-between p-4 border-[#000000]/10 border-b-2">
+              <p className="text-[#32332E]">Taxes & Fees</p>
+              <p className="text-[#32332E]">฿ 0.00</p>
+            </div>
           </div>
+          <div className="flex flex-col border-[#000000]/10 border-b-2">
+            <div className="p-4 flex flex-col gap-y-2">
+              <div className="flex justify-between">
+                <h1 className="text-[#32332E] text-3xl">Total</h1>
+                <p className="text-[#32332E] text-2xl">฿ 454.83</p>
+              </div>
+              <Link className="text-[#D48A3A]" href={''}>
+                Have a coupon code?
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="m-6">
+          <button className="w-full p-3 bg-[#FCB040] text-[#FFFFFF] hover:text-[#FCB040] hover:bg-[#FFFFFF] border-[#FCB040] border-2 rounded-lg">
+            Submit Secure Payment
+          </button>
         </div>
       </div>
     </div>
