600f422fdf859273eba85ce4ccac2f226f9fad82
santhitak <santhita.sk@gmail.com>
2023-04-07 03:25:27 +0700

    feat: add action

--- components/shared/PrimaryButton.tsx
+++ components/shared/PrimaryButton.tsx
@@ -1,12 +1,14 @@
 interface Props {
   children: React.ReactNode;
   customClass?: string;
+  action?: () => void;
 }
 
-const PrimaryButton = ({ children, customClass }: Props) => {
+const PrimaryButton = ({ children, customClass, action }: Props) => {
   return (
     <button
       className={`bg-primary-1 px-6 py-3 text-white rounded-md ${customClass}`}
+      onClick={action}
     >
       {children}
     </button>
